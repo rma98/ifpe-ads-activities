bb3f2fed17e82f885184c427d23fea79fda72224
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-11-07 23:48:40 -0300

    feat: Build Vue project and initial setup

---
+++ subjects/src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import QuestionsPage from '../views/QuestionsPage.vue';
+import UnitTestsPage from '../views/UnitTestsPage.vue';
+
+const routes = [
+    { path: '/home', name: 'Home', component: Home },
+    { path: '/question', name: 'Questions', component: QuestionsPage },
+    { path: '/unit-tests', name: 'UnitTests', component: UnitTestsPage },
+    { path: '/:pathMatch(.*)*', redirect: '/' }
+];
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+});
+
+export default router;
