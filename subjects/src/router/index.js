fefc8777b6d5fdc01dd3da08098545745ca990cf
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-11-08 00:34:53 -0300

    feat: Update router index.js

--- subjects/src/router/index.js
+++ subjects/src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 // import Home from '../views/Home.vue';
 import QuestionsPage from '../views/QuestionsPage.vue';
-//import UnitTestsPage from '../views/UnitTestsPage.vue';
+import UnitTestsPage from '../views/UnitTestsPage.vue';
 
 const routes = [
     // { path: '/home', name: 'Home', component: Home },
     { path: '/question', name: 'Questions', component: QuestionsPage },
-    // { path: '/unit-tests', name: 'UnitTests', component: UnitTestsPage },
+    { path: '/unit-tests', name: 'UnitTests', component: UnitTestsPage },
     { path: '/:pathMatch(.*)*', redirect: '/' }
 ];
 
