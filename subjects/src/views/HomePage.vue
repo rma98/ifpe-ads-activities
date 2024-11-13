5d40ca41d9734907f380aa7272915f7f99dbfe18
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-11-13 14:28:51 -0300

    feat: Update component HomePage.vue

--- subjects/src/views/HomePage.vue
+++ subjects/src/views/HomePage.vue
@@ -5,64 +5,99 @@
     <div class="profile-section">
       <img :src="fotoRobson" alt="Foto de Robson" class="profile-photo" />
       <p class="description">
-        Olá, meu nome é Robson, sou estudante do curso de Análise e Desenvolvimento de Sistemas. Esta página é dedicada às minhas atividades acadêmicas e projetos pessoais. Seja muito bem-vindo!
+        Olá, meu nome é Robson, sou estudante do curso de Análise e
+        Desenvolvimento de Sistemas. Esta página é dedicada às minhas atividades
+        acadêmicas e projetos pessoais. Seja muito bem-vindo!
       </p>
     </div>
 
     <div class="topics-grid">
       <div class="card">
         <h2><i class="fas fa-database"></i> Estrutura de Dados</h2>
-        <ul>
-          <li>
-            <router-link to="/complexity-of-algorithms" target="_blank">Complexidade de Algoritmos</router-link>
-          </li>
-        </ul>
+        <div class="card-actions">
+          <ul>
+            <li>
+              <router-link
+                to="/complexity-of-algorithms"
+                class="link-icon"
+                target="_blank"
+                ><i class="fas fa-chart-line"></i>Complexidade de
+                Algoritmos</router-link
+              >
+            </li>
+          </ul>
+        </div>
       </div>
 
       <div class="card">
         <h2><i class="fas fa-vial"></i> Teste de Software</h2>
-        <ul>
-          <li>
-            <router-link to="/questions" target="_blank">Perguntas</router-link>
-          </li>
-          <li>
-            <router-link to="/unit-tests" target="_blank">Testes de Unidade</router-link>
-          </li>
-        </ul>
+        <div class="card-actions">
+          <ul>
+            <li>
+              <router-link to="/questions" class="link-icon" target="_blank"
+                ><i class="fas fa-question-circle"></i>Perguntas</router-link
+              >
+            </li>
+            <li>
+              <router-link to="/unit-tests" class="link-icon" target="_blank"
+                ><i class="fas fa-check-circle"></i>Testes de
+                Unidade</router-link
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="card">
+        <h2><i class="fas fa-microchip"></i> Sistemas Embarcados</h2>
+        <div class="card-actions">
+          <button @click="openModal('Sistemas Embarcados')">Ementa</button>
+          <ul>
+            <li>
+              <router-link
+                to="/open-hardware"
+                class="link-icon"
+                target="_blank"
+              >
+                <i class="fas fa-microchip"></i> Hardware Livre
+              </router-link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
 
-    <div class="card">
-      <h2><i class="fas fa-microchip"></i> Sistemas Embarcados</h2>
-      <button @click="openModal('Sistemas Embarcados')">Ementa</button>
-    </div>
-
     <!-- Modal -->
-    <Modal v-if="showModal" :title="modalTitle" :show="showModal" @close="showModal = false">
+    <Modal
+      v-if="showModal"
+      :title="modalTitle"
+      :show="showModal"
+      @close="showModal = false"
+    >
       <component :is="modalContent"></component>
     </Modal>
   </div>
 </template>
 
 <script setup>
-import NavigationMenu from '../components/NavigationMenu.vue';
-import fotoRobson from '../assets/img/photo.jpg';
-import { ref } from 'vue';
-import Modal from '../components/Modal.vue';
-import EmentaSistemasEmbarcados from '../components/EmentaSistemasEmbarcados.vue';
+import NavigationMenu from "../components/NavigationMenu.vue";
+import fotoRobson from "../assets/img/photo.jpg";
+import { ref } from "vue";
+import Modal from "../components/Modal.vue";
+import EmentaSistemasEmbarcados from "../components/EmentaSistemasEmbarcados.vue";
 
 const showModal = ref(false);
-const modalTitle = ref('');
+const modalTitle = ref("");
 const modalContent = ref(null);
 
 function openModal(disciplina) {
   modalTitle.value = disciplina;
 
   // Verifica qual componente de ementa carregar
-  if (disciplina === 'Sistemas Embarcados') {
+  if (disciplina === "Sistemas Embarcados") {
     modalContent.value = EmentaSistemasEmbarcados;
   }
-  
+
   showModal.value = true;
 }
 </script>
@@ -72,10 +107,10 @@ function openModal(disciplina) {
   max-width: 1000px;
   margin: 2rem auto;
   padding: 2rem;
-  background: #F3F4F6;
+  background: #f3f4f6;
   border-radius: 10px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  color: #1F2937;
+  color: #1f2937;
 }
 
 .profile-section {
@@ -95,7 +130,7 @@ function openModal(disciplina) {
 
 .description {
   font-size: 1.6rem;
-  color: #1F2937;
+  color: #1f2937;
   text-align: center;
 }
 
@@ -118,17 +153,24 @@ function openModal(disciplina) {
   box-shadow: 0 6px 10px rgba(0, 0, 0, 0.15);
 }
 
+.card-actions {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-top: 1rem;
+}
+
 h2 {
   font-size: 2rem;
   margin-bottom: 1rem;
-  color: #3B82F6;
+  color: #3b82f6;
   display: flex;
   align-items: center;
   gap: 0.5rem;
 }
 
 button {
-  background-color: #10B981;
+  background-color: #10b981;
   color: #ffffff;
   border: none;
   border-radius: 5px;
@@ -158,15 +200,18 @@ li {
   margin-bottom: 0.5rem;
 }
 
-a {
-  text-decoration: none;
-  color: #3B82F6;
-  font-weight: 500;
+.link-icon {
+  display: inline-flex;
+  align-items: center;
+  gap: 0.5rem;
   font-size: 1.6rem;
+  color: #007acc;
+  margin-top: 1rem;
+  text-decoration: none;
 }
 
-a:hover {
-  text-decoration: underline;
+.link-icon:hover {
+  color: #005fa3;
 }
 
 @media (max-width: 600px) {
