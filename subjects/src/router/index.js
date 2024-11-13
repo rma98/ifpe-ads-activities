4b6acb7da72bb93c206384ba289cd5016580475d
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-11-13 02:24:28 -0300

    feat: Update router index.js

--- subjects/src/router/index.js
+++ subjects/src/router/index.js
@@ -1,14 +1,19 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import HomePage from '../views/HomePage.vue';
 import QuestionsPage from '../views/QuestionsPage.vue';
 import UnitTestsPage from '../views/UnitTestsPage.vue';
 import ComplexityOfAlgorithmsPage from '../views/ComplexityOfAlgorithmsPage.vue';
+import Horario5Page from '../views/Horario5Page.vue';
+import Horario6Page from '../views/Horario6Page.vue';
+
 
 const routes = [
-    { path: '/', name: 'Home', component: Home },
+    { path: '/', name: 'Home', component: HomePage },
     { path: '/questions', name: 'Questions', component: QuestionsPage },
     { path: '/unit-tests', name: 'UnitTests', component: UnitTestsPage },
     { path: '/complexity-of-algorithms', name: 'ComplexityOfAlgorithms', component: ComplexityOfAlgorithmsPage },
+    { path: '/horario-5-periodo', name: 'horario5', component: Horario5Page },
+    { path: '/horario-6-periodo', name: 'horario6', component: Horario6Page },
     { path: '/:pathMatch(.*)*', redirect: '/' }
 ];
 
