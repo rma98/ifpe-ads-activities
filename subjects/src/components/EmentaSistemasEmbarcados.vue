be3086bcbd78a49af9972c4e4a099c678f34bd59
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-11-12 22:56:21 -0300

    feat: Add component EmentaSistemasEmbarcados.vue

---
+++ subjects/src/components/EmentaSistemasEmbarcados.vue
@@ -0,0 +1,47 @@
+<!-- EmentaSistemasEmbarcados.vue -->
+<template>
+  <div class="ementa-content">
+    <ul>
+      <li>
+        <strong>Introdução a Sistemas Embarcados:</strong>
+        <p>Conceitos, classificações, aplicações, conhecimentos prévios.</p>
+      </li>
+      <li>
+        <strong>Novos tipos de Sistemas Embarcados:</strong>
+        <p>Tecnologias atuais para prototipação.</p>
+      </li>
+      <li>
+        <strong>Desenvolvimento de Sistemas Embarcados:</strong>
+        <p>Concepção, Implementação, teste.</p>
+      </li>
+      <li>
+        <strong>Mecanismos Emergentes em Sistemas Embarcados:</strong>
+        <p>Novas tecnologias propostas.</p>
+      </li>
+      <li>
+        <strong>Estudos Avançados sobre Sistemas Embarcados:</strong>
+        <p>Avaliação de projetos.</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.ementa-content ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.ementa-content li {
+  margin-bottom: 1rem;
+  font-size: 1.6rem;
+}
+
+.ementa-content p {
+  margin: 0.5rem 0 0;
+  font-size: 1.6rem;
+}
+</style>
