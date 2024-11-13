8578d3799d480efe7817542743bd69001dd43f00
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-11-13 02:25:40 -0300

    feat: Add components Horario5PPage.vue and Horario6Page.vue

---
+++ subjects/src/views/Horario5Page.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="schedule-container">
+    <NavigationMenu />
+    <h1 class="schedule-header">Horário Individual - 5º Período</h1>
+
+    <!-- Grid de cards combinados -->
+    <div class="schedule-grid">
+      <div
+        v-for="(discipline, index) in disciplines"
+        :key="index"
+        class="schedule-card"
+      >
+        <div class="card-header">
+          <h3>{{ discipline.name }}</h3>
+          <i class="fa fa-book"></i>
+          <!-- Ícone da disciplina -->
+        </div>
+        <div class="card-body">
+          <div class="schedule-info">
+            <!-- Exibe os dias e horários -->
+            <p v-for="(session, i) in discipline.schedule" :key="i">
+              <span class="day-time"
+                >{{ session.day }}: {{ session.time }}</span
+              >
+            </p>
+          </div>
+          <!-- Exibe a descrição da disciplina -->
+          <p class="discipline-description">
+            {{ discipline.description }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavigationMenu from "../components/NavigationMenu.vue";
+
+export default {
+  components: {
+    NavigationMenu,
+  },
+  data() {
+    return {
+      disciplines: [
+        {
+          name: "ELTCC 1",
+          schedule: [{ day: "Segunda-feira", time: "07:45 às 09:15" }],
+          description:
+            "Orientações para Elaboração do Tcc 1 - Delano Hélio Oliveira",
+        },
+        {
+          name: "PDSWEB II",
+          schedule: [
+            { day: "Segunda-feira", time: "09:35 às 10:19" },
+            { day: "Terça-feira", time: "07:45 às 10:19" },
+          ],
+          description:
+            "Projeto de Desnvolvimento de Sistemas Web II - Leonardo de Souza Lima",
+        },
+        {
+          name: "SISEMB",
+          schedule: [
+            { day: "Terça-feira", time: "10:20 às 11:50" },
+            { day: "Quinta-feira", time: "10:20 às 11:50" },
+          ],
+          description: "Sistemas Embarcados - David Edson Ribeiro",
+        },
+        {
+          name: "TOPINF",
+          schedule: [{ day: "Quarta-feira", time: "07:45 às 09:15" }],
+          description:
+            "Tópicos Avançados em Tecnologia da Informação - Diogo Lopes da Silva",
+        },
+        {
+          name: "INTART",
+          schedule: [{ day: "Quarta-feira", time: "09:35 às 11:50" }],
+          description:
+            "Inteligência Artificial - Tiago Pessoa Ferreira de Lima",
+        },
+        {
+          name: "INTMAQU",
+          schedule: [
+            { day: "Quinta-feira", time: "08:30 às 10:19" },
+            { day: "Sexta-feira", time: "07:45 às 09:15" },
+          ],
+          description: "Intereface Homem-máquina - Sionise Rocha Gomes",
+        },
+        {
+          name: "TSOFTWARE",
+          schedule: [{ day: "Sexta-feira", time: "09:35 às 11:50" }],
+          description: "Teste de Software - Delano Hélio Oliveira",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ subjects/src/views/Horario6Page.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="schedule-container">
+    <h1 class="schedule-header">Horário Individual - 6º Período</h1>
+
+    <!-- Grid de cards combinados -->
+    <div class="schedule-grid">
+      <div
+        v-for="(discipline, index) in disciplines"
+        :key="index"
+        class="schedule-card"
+      >
+        <div class="card-header">
+          <h3>{{ discipline.name }}</h3>
+          <i class="fa fa-book"></i> <!-- Ícone da disciplina -->
+        </div>
+        <div class="card-body">
+          <div class="schedule-info">
+            <!-- Exibe os dias e horários -->
+            <p v-for="(session, i) in discipline.schedule" :key="i">
+              <span class="day-time">{{ session.day }}: {{ session.time }}</span>
+            </p>
+          </div>
+          <!-- Exibe a descrição da disciplina -->
+          <p class="discipline-description">
+            {{ discipline.description }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      disciplines: [
+        {
+          name: 'PDSWEB III',
+          schedule: [
+            { day: 'Segunda-feira', time: '07:45 às 09:15' },
+            { day: 'Quarta-feira', time: '08:30 às 09:15' }
+          ],
+          description: 'Projeto de Desenvolvimento de Sistemas Web III - Leonardo de Souza Lima'
+        },
+        {
+          name: 'ESTINF',
+          schedule: [
+            { day: 'Terça-feira', time: '08:30 às 09:15' },
+            { day: 'Quinta-feira', time: '08:30 às 09:15' }
+          ],
+          description: 'Estruturas para Internet - Fábio Costa'
+        },
+        {
+          name: 'ELTCC 2',
+          schedule: [
+            { day: 'Segunda-feira', time: '08:30 às 09:15' },
+            { day: 'Sexta-feira', time: '09:35 às 10:19' }
+          ],
+          description: 'Orientações para Elaboração do TCC 2 - Delano Hélio Oliveira'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
