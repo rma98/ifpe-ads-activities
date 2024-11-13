1aa3e5e6386b892f6d697063a541708a7966f46e
Robson Albuquerque <robalbuquerque98@gmail.com>
2024-11-13 02:22:03 -0300

    feat: Update file main.js

--- subjects/src/main.js
+++ subjects/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 
 import './assets/styles/global.css';
+import './assets/styles/schedule.css'
 import '@fortawesome/fontawesome-free/css/all.css';
 
 const app = createApp(App);
