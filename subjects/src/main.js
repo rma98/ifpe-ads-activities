b87f85b0ee19ec546441b2db69025c848db9adb6
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-03-31 23:00:52 -0300

    feat: Update Setup initial Vite

--- subjects/src/main.js
+++ subjects/src/main.js
@@ -6,6 +6,17 @@ import './assets/styles/global.css';
 import './assets/styles/schedule.css'
 import '@fortawesome/fontawesome-free/css/all.css';
 
+// Importando FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+// Importando os ícones específicos
+import { faProjectDiagram, faQuestionCircle, faTasks, faClipboardList, faListUl, faUsers, faSearch } from '@fortawesome/free-solid-svg-icons';
+
+// Adicionando os ícones à biblioteca
+library.add(faProjectDiagram, faQuestionCircle, faTasks, faClipboardList, faListUl, faUsers, faSearch);
+
 const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(router);
 app.mount('#app');
