ecfbc23fdb011031e3f1f8ab23f1167f639070de
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-04-29 20:07:56 -0300

    feat: Add component UserCard.vue

---
+++ subjects/src/components/UserCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <li>
+        <article>
+            <h3>{{ user.name }}</h3>
+            <p>Email: {{ user.email }}</p>
+            <p>Empresa: {{ user.company.name }}</p>
+        </article>
+    </li>
+</template>
+
+<script setup>
+defineProps(['user']);
+</script>
+
+<style scoped>
+article {
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    padding: 1rem;
+    margin-bottom: 1rem;
+    background-color: #f9f9f9;
+}
+</style>
